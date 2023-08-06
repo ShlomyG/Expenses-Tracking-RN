f4185380b63f2bc71b54a36cec7e591a78c602a7
Shlomy Gerstner <shlomyg@moveo.co.il>
2023-08-06 12:49:45 +0300

    change app name and clean app file

--- App.tsx
+++ App.tsx
@@ -1,16 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-
 import {SafeAreaView, StyleSheet} from 'react-native';
-
 import {navigationReadiness, navigationRef} from './src/navigation/RootNavigation';
 import AppNavigator from './src/navigation/AppNavigator';
 import {Provider} from 'react-redux';
